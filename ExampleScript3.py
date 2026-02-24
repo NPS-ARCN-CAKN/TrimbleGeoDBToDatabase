ef907f0dcae3ec7249ab583d460b105c85c80224
Nick Bywater <nicklas_bywater@ios.doi.gov>
2026-02-24 12:12:01 -0900

    - Remove references to external CSV file and csv.DictReader.

--- ExampleScript3.py
+++ ExampleScript3.py
@@ -16,36 +16,20 @@
 # Example script 'ExampleScript3.py':
 
 import arcpy
-import csv
 import TrimbleGeoDBToDatabase
 import TestTrimbleGeoDB
 
 from TrimbleGeoDBToDatabase import Continuous
 
 def Transform():
-    # Deployment processing
-    GEO_DB_PATH = "C:/fake_dir/fake_1.gdb"
+
     arcpy.env.workspace = GEO_DB_PATH
 
+    # Deployment processing
     TrimbleGeoDBToDatabase.ExportContinuousJoined(Continuous.DEPLOYMENT_INSERT, '2023-09-08', '2023-09-25')
 
     # Retrieval processing
-
-    # Get deployment site names and dates from a CSV file, for which
-    # the retrievals make sense.
-    DEPLOY_INPUT = 'C:/fake_dir/input.csv'
-
-    GEO_DB_PATH = "C:/fake_dir/fake_2.gdb"
-    arcpy.env.workspace = GEO_DB_PATH
-
-    with open(DEPLOY_INPUT, mode='r', newline='') as file:
-        # In this case, the CSV file has a tab delimiter.
-        csv_reader = csv.DictReader(file, delimiter='\t')
-
-        # Pass in the CSV input as a csv.DictReader.
-        # By default, the output does not keep the comments. See doc
-        # notes for function 'ExportContinuousJoined'.
-        TrimbleGeoDBToDatabase.ExportContinuousJoined(Continuous.RETRIEVAL_UPDATE, '2024-05-24', '2024-06-21', csv_reader)
+    TrimbleGeoDBToDatabase.ExportContinuousJoined(Continuous.RETRIEVAL_UPDATE, '2024-05-24', '2024-06-21')
 
 if __name__ == "__main__":
     Transform()
